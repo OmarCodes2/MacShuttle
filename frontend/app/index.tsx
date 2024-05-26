d8ba1b7e6b8c7b0675f286745f4f5c31c093d9bc
OmarCodes2 <omarcodes2@gmail.com>
2024-05-25 22:01:17 -0400

    add longitude and latitude to app

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function Home() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>{text}</Text>
     </View>
   );
 }
@@ -19,5 +43,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     color: '#000',
+    margin: 10,
   },
 });
