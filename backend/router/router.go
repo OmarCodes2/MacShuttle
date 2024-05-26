06b34557396fb23c8a8cb0ee0115b409d6450fbb
OmarCodes2 <omarcodes2@gmail.com>
2024-05-26 12:29:26 -0400

    add direction

--- backend/router/router.go
+++ backend/router/router.go
@@ -10,6 +10,7 @@ type LocationData struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 	Timestamp int64   `json:"timestamp"`
+	Direction string  `json:"direction"`
 }
 
 func InitializeRouter() *http.ServeMux {
