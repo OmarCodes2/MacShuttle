4e86894f9df11c7e372bd5ba2531c64df8a9a13b
OmarCodes2 <omarcodes2@gmail.com>
2024-06-08 19:26:16 -0400

    add websocket functionality with dummy data

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,8 @@ module github.com/OmarCodes2/MacShuttle
 go 1.20
 
 require (
+	github.com/gorilla/websocket v1.5.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/net v0.23.0 // indirect
 )
-
--- backend/main.go
+++ backend/main.go
@@ -6,11 +6,44 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/OmarCodes2/MacShuttle/router"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// Dummy ETA data
+		dummyData := map[string]string{
+			"stop1": "ETA: 2 minutes",
+			"stop2": "ETA: 5 minutes",
+		}
+
+		err = conn.WriteJSON(dummyData)
+		if err != nil {
+			log.Println("Error writing JSON:", err)
+			break
+		}
+
+		// Send updates every 5 seconds
+		time.Sleep(5 * time.Second)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -35,6 +68,7 @@ func main() {
 	fmt.Println("Successfully connected to the PostgreSQL database")
 
 	r := router.InitializeRouter(db)
+	r.HandleFunc("/ws", wsHandler)
 	fmt.Println("Server is running on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
