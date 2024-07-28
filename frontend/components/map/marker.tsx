8a3a5f8ca51f938333a2e20a2530c2dc63a38597
charcoalyy <alina.yzeng@gmail.com>
2024-07-28 16:53:12 -0400

    multi-type map markers

--- frontend/components/map/marker.tsx
+++ frontend/components/map/marker.tsx
@@ -1,12 +1,28 @@
 import { Marker } from 'react-native-maps'
 import MarkerDefaultIcon from '../../assets/map/marker-default.svg'
+import MarkerSelectedIcon from '../../assets/map/marker-selected.svg'
+import MarkerMovingIcon from '../../assets/map/marker-moving.svg'
+import { useMemo } from 'react'
+import { markerType } from '@/constants/map/types'
 
 interface MapMarkerProps {
   latitude: number
   longitude: number
+  type?: markerType
 }
 
-const MapMarker = ({ latitude, longitude }: MapMarkerProps) => {
+const MapMarker = ({ latitude, longitude, type }: MapMarkerProps) => {
+  const markerIcon = useMemo(() => {
+    switch (type) {
+      case 'moving':
+        return <MarkerMovingIcon width={35} height={35} />
+      case 'selected':
+        return <MarkerSelectedIcon width={35} height={35} />
+      case 'default':
+      default:
+        return <MarkerDefaultIcon width={35} height={35} />
+    }
+  }, [type])
   return (
     <Marker coordinate={{ latitude, longitude }}>
       <MarkerDefaultIcon width={35} height={35} />
